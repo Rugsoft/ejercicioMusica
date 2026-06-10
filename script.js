92b9a4261789d968869c973f565ac254b104b541
Rugsoft <futuroelectronico@gmail.com>
2026-06-10 10:59:31 +0200

    añadido estilos y logica basica

---
+++ script.js
@@ -0,0 +1,61 @@
+
+const pistas = document.querySelectorAll(".pista");
+
+let pistaActual = null;
+let audioActual = null;
+
+pistas.forEach((pista) => {
+    pista.addEventListener("click", () => {
+
+        const audioId = pista.getAttribute("data-audio");
+        const audio = document.getElementById(audioId);
+
+        if (pistaActual === pista) {
+            
+            detenerReproduccion();
+        } else {
+            if (audioActual) {
+                
+                const confirmar = window.confirm("¿Seguro que quieres parar la pista actual y reproducir esta?");
+
+                if (confirmar) {
+
+                    detenerReproduccion();
+                    audio.play();
+                    pista.classList.add("activa");
+                    pistaActual = pista;
+                    audioActual = audio;
+                    audio.onended = () => {
+                        detenerReproduccion();
+                    };
+                }
+
+            } else {
+                audio.play();
+                pista.classList.add("activa");
+                pistaActual = pista;
+                audioActual = audio;
+                audio.onended = () => {
+                    detenerReproduccion();
+                };
+
+            }  
+        }
+    });
+});
+
+function detenerReproduccion() {
+
+    if (audioActual) {
+
+        audioActual.pause();
+        audioActual.currentTime = 0; 
+    }
+    if (pistaActual){
+
+        pistaActual.classList.remove("activa");
+    }
+
+    audioActual = null;
+    pistaActual = null;
+}
